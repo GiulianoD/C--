2e4eec84acd3a5fa08ac2f910234cbe9dc963f18
Giuliano de Paulo Demoner <42439057+GiulianoD@users.noreply.github.com>
2022-11-08 16:05:19 -0300

    added input limit
    + comments

--- src/pass_get.c
+++ src/pass_get.c
@@ -5,12 +5,13 @@
 #define ASCII_BKSPC 8 // backspack on ascII
 #define ASCII_LNBRK 10 // line break on ascII
 #define ASCII_ENTER 13 // enter on ascII
+#define PSS_LIMIT 30 // limit size of the password (in characters)
 #define PSS_CH '*' // character to be shown as passwork mask
-#define CLR "cls" // string to be passed as param to system() in order to clear the console
+#define CLR "cls" // string to be passed as param to system() in order to clear the console (differs on each OS)
 
 int main(){
     char c;
-    char *password = malloc(30);
+    char *password = malloc(PSS_LIMIT);
     char current_size = 0;
     int aux;
 
@@ -21,11 +22,14 @@ int main(){
             password[current_size] = 0;
             break;
         }
+
         if (c == ASCII_BKSPC){ // verify if the char entered was a backspace
-            if (current_size)current_size--;
-        } else { // if we manage to get in here, so the character typed is part of the password
-            password[current_size] = c;
-            current_size++;
+            if (current_size) current_size--;
+
+        } else // if we manage to get in here, so the character typed is part of the password
+
+        if (current_size < PSS_LIMIT){ // check if the pass have reached the limit already
+            password[current_size++] = c;
         }
 
         system(CLR);
